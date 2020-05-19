b7df288928022379ac625094f656d448a5b12698
diomcferreira <diomc.ferreira@gmail.com>
2020-05-19 12:21:24 +0000

    added timer

--- main.js
+++ main.js
@@ -59,3 +59,10 @@ function resetBoard() {                         //
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 
+var timer = new Timer();                       //timer function
+timer.start();
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#countUp').html(timer.getTimeValues().toString());
+});
+    
+
